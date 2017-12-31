27347e57d2c834bae37248d927ffded17e64656f
MxWild <mxwild@gmail.com>
2017-12-31 13:44:07 +0300

    Complete first task (not optional)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,53 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+
+        // поиск свободного элемента
+        if (get(r.uuid) == null) {
+            storage[size++] = r;
+        }
     }
 
     Resume get(String uuid) {
+
+            for (int i = 0; i < size; i++) {
+                if(storage[i].uuid.equals(uuid)) return storage[i];
+            }
+
         return null;
     }
 
     void delete(String uuid) {
+
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) storage[i] = null;
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] newStorage = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            newStorage[i] = storage[i];
+        }
+        return newStorage;
     }
 
     int size() {
-        return 0;
+        return this.size;
     }
 }
