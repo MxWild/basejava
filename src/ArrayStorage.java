0ea6e9c6473b956741477cccbce2d9cfd7422614
MxWild <mxwild@gmail.com>
2018-01-10 22:35:38 +0300

    Add optional

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -33,7 +35,11 @@ public class ArrayStorage {
     void delete(String uuid) {
 
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) storage[i] = null;
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+            }
         }
 
     }
