79008f48d47ac0719ef4c356f85b21a45d36b704
MxWild <mxwild@gmail.com>
2018-01-12 19:01:23 +0300

    Fix GetAll() method

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -46,11 +48,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] newStorage = new Resume[size];
-        for (int i = 0; i < size; i++) {
-            newStorage[i] = storage[i];
-        }
-        return newStorage;
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
