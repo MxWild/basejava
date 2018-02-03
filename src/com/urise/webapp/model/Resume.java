b610c904ca94aa24576e830acb95a1c010f73013
MxWild <mxwild@gmail.com>
2018-02-03 20:47:38 +0300

    HW4 - add constructor to Resume class

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -1,5 +1,7 @@
 package com.urise.webapp.model;
 
+import java.util.UUID;
+
 /**
  * com.urise.webapp.model.Resume class
  */
@@ -8,13 +10,22 @@ public class Resume implements Comparable<Resume> {
     // Unique identifier
     private String uuid;
 
+    public Resume() {
+        this(UUID.randomUUID().toString());
+    }
+
+    public Resume(String uuid) {
+        this.uuid = uuid;
+    }
+
     public String getUuid() {
         return uuid;
     }
 
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
+// remove on HW4
+//    public void setUuid(String uuid) {
+//        this.uuid = uuid;
+//    }
 
     @Override
     public String toString() {
