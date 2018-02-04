4c9b4cc4720a13a73c82ce4fcd7545e9cadf8571
MxWild <mxwild@gmail.com>
2018-02-04 20:34:35 +0300

    HW4 - add AbstractArrayStorageTest

---
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,37 @@
+package com.urise.webapp.storage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AbstractArrayStorageTest {
+
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+}
