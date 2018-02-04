db8098e3f08a6194ddcf274ff9dd3e9cda558546
MxWild <mxwild@gmail.com>
2018-02-04 22:15:18 +0300

    HW4 - add MainReflection class

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -1,11 +1,17 @@
 package com.urise.webapp.storage;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class AbstractArrayStorageTest {
 
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
 
     @Test
     public void size() {
