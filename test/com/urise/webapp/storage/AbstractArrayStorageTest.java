8ce68dbf035d82cceadcc62609604540df9e248b
MxWild <mxwild@gmail.com>
2018-02-05 22:22:50 +0300

    HW4 - add size test

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -1,20 +1,33 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.model.Resume;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.UUID;
 
 import static org.junit.Assert.*;
 
 public class AbstractArrayStorageTest {
 
+    private Storage storage = new ArrayStorage();
+
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
 
     @Before
     public void setUp() throws Exception {
-
+        storage.clear();
+        storage.save(new Resume(UUID_1));
+        storage.save(new Resume(UUID_2));
+        storage.save(new Resume(UUID_3));
     }
 
     @Test
     public void size() {
+        Assert.assertEquals(3, storage.size());
     }
 
     @Test
