5c633668ee852ed6017266618944d7ea25f334cb
MxWild <mxwild@gmail.com>
2018-05-12 23:12:30 +0300

    Add ListSection class

--- src/com/urise/webapp/model/ListSection.java
+++ src/com/urise/webapp/model/ListSection.java
@@ -1,4 +1,40 @@
 package com.urise.webapp.model;
 
+import java.util.List;
+import java.util.Objects;
+
 public class ListSection extends Section {
+
+    private List<String> list;
+
+    public ListSection(List<String> list) {
+        Objects.requireNonNull(list, "Must be set");
+        this.list = list;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ListSection that = (ListSection) o;
+
+        return list.equals(that.list);
+    }
+
+    @Override
+    public int hashCode() {
+        return list.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "ListSection{" +
+                "list=" + list +
+                '}';
+    }
 }
