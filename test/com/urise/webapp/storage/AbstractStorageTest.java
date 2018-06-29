bd906e4d45e5b3ff6a4ac9a15b5f21a838bb3c28
MxWild <mxwild@gmail.com>
2018-06-29 21:36:59 +0300

    HW13 - add Config singleton class

--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -1,5 +1,6 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.Config;
 import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.NotExistStorageException;
 import com.urise.webapp.model.*;
@@ -13,7 +14,7 @@ import java.util.List;
 
 public abstract class AbstractStorageTest {
 
-    protected static final File STORAGE_DIR = new File("./storage");
+    protected static final File STORAGE_DIR = Config.getInstance().getStorageDir();
 
     private static final String UUID_1 = "uuid1";
     private static final String NAME_1 = "Name1";
