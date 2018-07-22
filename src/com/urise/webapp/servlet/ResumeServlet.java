4b90fe03eea58392dfefbcf4261b8b2bb8007e36
mxwild@gmail.com <Uhfybwf#DCodeGit>
2018-07-22 14:39:20 +0300

    HW17 - test

--- src/com/urise/webapp/servlet/ResumeServlet.java
+++ src/com/urise/webapp/servlet/ResumeServlet.java
@@ -27,6 +27,7 @@ public class ResumeServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //TODO Добавить разные страницы и css Bootstrap
         request.setCharacterEncoding("UTF-8");
         String uuid = request.getParameter("uuid");
         String fullName = request.getParameter("fullName");
